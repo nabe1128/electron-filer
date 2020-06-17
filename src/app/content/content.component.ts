e7b5b107c054f4f10fb26019d0fe4fa702a0b2aa
watanabe <t.watanabe1128@gmail.com>
2020-06-17 16:44:53 +0900

    ファイル一覧の表示処理実装
    ファイル名、サイズ、最終更新日、作成日の4項目を表示する

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FileService } from '../core/services';
+import { File } from '../models/file';
 
 @Component({
   selector: '[filer-content]',
@@ -8,6 +9,10 @@ import { FileService } from '../core/services';
 })
 export class ContentComponent implements OnInit {
 
+  currentDirFiles: File[] = [];
+
+  private _today = new Date();
+
   constructor(
     private readonly _fileService: FileService
   ) { }
@@ -15,13 +20,40 @@ export class ContentComponent implements OnInit {
   ngOnInit(): void {
     this._setFilesObserver();
 
+    // TODO 2回リクエストを投げないで済む方法模索
     this._fileService.getHomeFiles(false);
+    setTimeout(() => {
+      this._fileService.getHomeFiles(false);
+    }, 0);
   }
 
   private _setFilesObserver() {
     this._fileService.files$.subscribe((fileList) => {
-      console.log(fileList);
+      this.currentDirFiles = fileList;
     });
   }
 
+  formatSize(size: number): string {
+    // GB
+    if (size > 1000000000) {
+      const gb = size / 1000000000;
+      return `${gb.toFixed(1)}GB`;
+    }
+    // MB
+    else if (size > 1000000) {
+      const mb = size / 1000000;
+      return `${mb.toFixed(1)}MB`;
+    }
+    // KB
+    else {
+      const kb = size / 1000;
+      return `${kb.toFixed(1)}KB`;
+    }
+  }
+
+  isToday(date: Date): boolean {
+    return this._today.getFullYear() === date.getFullYear()
+      && this._today.getMonth() === date.getMonth()
+      && this._today.getDate() === date.getDate();
+  }
 }
