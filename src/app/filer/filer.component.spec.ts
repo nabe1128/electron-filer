9c2219b6941684a11f9567f21e9856f3c85fa0e5
watanabe <t.watanabe1128@gmail.com>
2021-01-19 23:13:22 +0900

    Angular11に更新

--- src/app/filer/filer.component.spec.ts
+++ src/app/filer/filer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FilerComponent } from './filer.component';
 
@@ -6,7 +6,7 @@ describe('FilerComponent', () => {
   let component: FilerComponent;
   let fixture: ComponentFixture<FilerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FilerComponent ]
     })
